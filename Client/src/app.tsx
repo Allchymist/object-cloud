1f67f5e9f8e19c649701405816e703cc3301136c
zThiagoR <thi.magalha13@gmail.com>
2022-05-31 17:27:55 -0300

    refactor: Removing styled-components

--- Client/src/app.tsx
+++ Client/src/app.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ThemeProvider, DefaultTheme } from 'styled-components'
 
 import { NotFound } from "./pages/NotFound";
 import { DashBoard } from "./pages/private/dashboard";
@@ -9,36 +8,23 @@ import { Login } from "./pages/public/Login";
 import { Register } from "./pages/public/Register";
 import { ForgotPassword } from "./pages/public/ForgotPassword";
 
-import dark from './styles/themes/dark';
-import light from './styles/themes/light';
-
-import GlobalStyle from './styles/global';
 import Header from './components/Global/Header';
-import usePersistedState from "./utils/usePersistedState";
-import { Footer } from "./components/Global/Footer";
+import Footer from "./components/Global/Footer";
 
 export function App() {
-  const [theme, setTheme] = usePersistedState<DefaultTheme>('@theme', light);
-
-  const toggleTheme = () => {
-    setTheme(theme.name === 'light' ? dark : light);
-  }
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle/>
-      <Router>
-        <Header toggleTheme={toggleTheme}/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/dashboard" element={<DashBoard />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
-    </ThemeProvider>
+    </Router>
   );
 }
