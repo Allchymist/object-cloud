5d3efda4e3471e4ef1da3aa16e00a723916978c0
zThiagoR <thi.magalha13@gmail.com>
2022-05-31 17:28:15 -0300

    Refactor: Change export to export default

--- Client/src/components/Global/Footer/index.tsx
+++ Client/src/components/Global/Footer/index.tsx
@@ -1,6 +1,6 @@
 import './footer.css'
 
-export function Footer() {
+export default function Footer() {
   return (
     <footer className="Footer">
       <p>Object Panel&reg; &copy; 2022 - {new Date().getUTCFullYear()}</p>
