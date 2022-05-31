911cff274df2be10bc6a92cc840809ac04dce16d
zThiagoR <thi.magalha13@gmail.com>
2022-05-31 16:26:48 -0300

    Initial configuration TailwindCSS

---
+++ Client/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-primary": "#292b2f",
+        "dark-secondary": "#383e42",
+        "dark-terciary": "#40444b",
+        "light-primary": "#447ac2",
+        "light-secondary": "#445dc2",
+        "light-terciary": "#e1e5eb",
+        'content-dark': '#222',
+        'content-light': '#fff',
+      }
+    },
+  },
+  plugins: [],
+}
